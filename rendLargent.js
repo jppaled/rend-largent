8b40537361c2b2a901b9096e0bba4df3295aeec0
jppaled <22444128+jppaled@users.noreply.github.com>
2020-12-02 22:58:06 +0000

    fix calculation

--- rendLargent.js
+++ rendLargent.js
@@ -1,13 +1,14 @@
+// currencies possibility in €
 const currencies = [
-    "500",
-    "200",
-    "100",
-    "50",
-    "20",
-    "10",
-    "5",
-    "2",
-    "1",
+    "500.00",
+    "200.00",
+    "100.00",
+    "50.00",
+    "20.00",
+    "10.00",
+    "5.00",
+    "2.00",
+    "1.00",
     "0.50",
     "0.20",
     "0.10",
@@ -16,16 +17,17 @@ const currencies = [
     "0.01"
 ];
 
-const changeDetail = {
-    "500": 0,
-    "200": 0,
-    "100": 0,
-    "50": 0,
-    "20": 0,
-    "10": 0,
-    "5": 0,
-    "2": 0,
-    "1": 0,
+// initial change detail array
+let changeDetail = {
+    "500.00": 0,
+    "200.00": 0,
+    "100.00": 0,
+    "50.00": 0,
+    "20.00": 0,
+    "10.00": 0,
+    "5.00": 0,
+    "2.00": 0,
+    "1.00": 0,
     "0.50": 0,
     "0.20": 0,
     "0.10": 0,
@@ -35,7 +37,8 @@ const changeDetail = {
 };
 
 const error = document.getElementById('error');
-const ul = document.getElementById('change');
+const ul = document.getElementById('changeDetail');
+const totalChange = document.getElementById('totalChange');
 
 const priceInput = document.getElementById("price");
 const moneyInput = document.getElementById("money");
@@ -45,64 +48,85 @@ function descSort(a, b) {
 }
 
 function rendLargent(price, money) {
-    if(isNaN(price) || price < 0) {
-        error.innerHTML = "invalid price";
-    } else if(isNaN(money) || money < 0) {
-        error.innerHTML = "invalid money";
-    } else if(money < price) {
-        error.innerHTML = "not enough money";
-    } else {
-        let change = money - price;
+	if (isNaN(price) || price < 0) {
+		error.innerHTML = "invalid price";
+	} else if (isNaN(money) || money < 0) {
+		error.innerHTML = "invalid money";
+	} else if (money < price) {
+		error.innerHTML = "not enough money";
+	} else {
+		let change = money - price;
 
-        if(change === 0) {
-            error.innerHTML = "nothing to give back!";
-        } else {
-           let result = changeDetailCalculation(change);
+		if (change === 0) {
+			error.innerHTML = "nothing to give back!";
+		} else {
+			change = Number(change).toFixed(2);
+			let result = changeDetailCalculation(change);
 
-           render(result);
-        }
-    }
+			render(result, change);
+		}
+	}
 }
 
+function searchBestReturn(change) {
+	let currenciesSorted = currencies.sort(descSort);
+	
+	for (let i = 0; i < currenciesSorted.length; i++) {
+		currency = Number(currenciesSorted[i]);
+		
+		if (change >= currency) {
+			return currency;
+		}
+	}
+
+	return false;
+}
 function changeDetailCalculation(change) {
-    while (change !== "0.00") {
-        currencies.sort(descSort).forEach(currency => {
-            if (change >= currency) {
-                change = (change - currency).toFixed(2);
+    while (change > 0.00) {
+		bestReturn = searchBestReturn(change);
+		
+		change = (change - bestReturn).toFixed(2);
+		
+		let changeDetailKey = bestReturn.toFixed(2).toString();
+		changeDetail[changeDetailKey]++;
 
-                changeDetail[currency.toString()]++
-            }
-        })
     }
 
-    for (currency in changeDetail) {
-        if(changeDetail[currency] === 0) {
-            delete changeDetail[currency];
-        }
-    }
-
-    return changeDetail;
+    return cleanChangeDetail();
 }
 
-function render(changeDetail) {
+function render(changeDetail, change) {
+	totalChange.innerHTML += "Total change: "+ change;
+	
     for (currency in changeDetail) {
         ul.innerHTML += "<li>" + currency +" € x " + changeDetail[currency] + "</li>";
     }
 }
 
+function cleanChangeDetail() {
+	for (currency in changeDetail) {
+		if (changeDetail[currency] === 0) {
+            delete changeDetail[currency];
+        }
+    }
+	
+	return changeDetail;
+}
+
 function reset() {
-    for(detail in changeDetail) {
+    for (detail in changeDetail) {
         changeDetail[detail] = 0;
     }
 
     ul.innerHTML = "";
+	totalChange.innerHTML = "";
     error.innerHTML = "";
 }
 
 function main() {
     reset();
 
-    if(priceInput.checkValidity() && moneyInput.checkValidity()) {
-        rendLargent(priceInput.value, moneyInput.value);
+    if (priceInput.checkValidity() && moneyInput.checkValidity()) {
+        rendLargent(Number(priceInput.value), Number(moneyInput.value));
     }
 }
